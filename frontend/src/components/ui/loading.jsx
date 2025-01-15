8f22b256e7878feaf3594330b6f9d75df553ba32
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2025-01-16 00:20:03 +0200

    Add loading component with rotating icon and CSS animation

---
+++ frontend/src/components/ui/loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+    return ( 
+        <div className="w-full h-full flex justify-center">
+            <img className="w-36 h-36 mt-[15%] rounded-full loading-icon" src="/logo.jpeg" alt="loading icon" />
+        </div>
+     );
+}
+ 
+export default Loading;
