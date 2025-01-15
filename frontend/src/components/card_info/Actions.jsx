f75b34908117475f9457e0daa85cdcb744a40b6e
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2025-01-15 05:29:00 +0200

    Update button text size in Actions component for better readability

--- frontend/src/components/card_info/Actions.jsx
+++ frontend/src/components/card_info/Actions.jsx
@@ -1,9 +1,9 @@
 const Actions = () => {
     return (
         < div className=" flex flex-col mt-7 gap-4 " >
-            <button className="border h-9 text-[12px] border-black"> Freeze Card</button>
-            <button className="border h-9 text-[12px] border-black"> Replace Card</button>
-            <button className="border h-9 text-[12px] border-black"> Cancel Card</button>
+            <button className="border h-9 text-[12.5px] border-black"> Freeze Card</button>
+            <button className="border h-9 text-[12.5px] border-black"> Replace Card</button>
+            <button className="border h-9 text-[12.5px] border-black"> Cancel Card</button>
         </div >
 
     );
