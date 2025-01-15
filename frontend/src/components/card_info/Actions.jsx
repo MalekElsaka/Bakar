f483df1ef586679e5dae3231735d9e284abeac7a
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2025-01-16 00:19:17 +0200

    Enhance button styles with hover effects for improved user interaction

--- frontend/src/components/card_info/Actions.jsx
+++ frontend/src/components/card_info/Actions.jsx
@@ -1,9 +1,9 @@
 const Actions = () => {
     return (
         < div className=" flex flex-col mt-7 gap-4 " >
-            <button className="border h-9 text-[12.5px] border-black"> Freeze Card</button>
-            <button className="border h-9 text-[12.5px] border-black"> Replace Card</button>
-            <button className="border h-9 text-[12.5px] border-black"> Cancel Card</button>
+            <button className="border h-9 text-[12.5px] border-black hover:bg-black hover:text-white transition-colors duration-300"> Freeze Card</button>
+            <button className="border h-9 text-[12.5px] border-black hover:bg-black hover:text-white transition-colors duration-300"> Replace Card</button>
+            <button className="border h-9 text-[12.5px] border-black hover:bg-black hover:text-white transition-colors duration-300"> Cancel Card</button>
         </div >
 
     );
