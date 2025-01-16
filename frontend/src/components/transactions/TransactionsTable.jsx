d271f67faf9a6894d6422c6ba2741d9da63a6ce6
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2025-01-16 15:26:45 +0200

    Refactor TransactionsTable component to simplify column definitions and improve readability

--- frontend/src/components/transactions/TransactionsTable.jsx
+++ frontend/src/components/transactions/TransactionsTable.jsx
@@ -10,28 +10,26 @@ import {
 import SortIcon from './SortIcon';
 import { formatDate, mapStatus } from '../../lib/utils';
 
-
 const TransactionsTable = ({ currentTransactions, sortConfig, requestSort }) => {
-    const columns = [
-        { key: 'amount', label: 'Amount', width: '21%' },
-        { key: 'currency', label: 'Currency', width: '20%' },
-        { key: 'cardholder', label: 'Cardholder', width: '25%' },
-        { key: 'status', label: 'Status', width: '24%' },
-        { key: 'created_at', label: 'Created', width: '9%' }
-    ];
-
     return (
         <Table>
             <TableHeader>
                 <TableRow>
-                    {columns.map(column => (
-                        <TableHead 
-                            key={column.key}
-                            className={`w-[${column.width}] text-nowrap text-[15px] ${column.key === 'created_at' ? 'pr-0' : ''}`}
-                        >
-                            {column.label} <SortIcon columnName={column.key} sortConfig={sortConfig} requestSort={requestSort} />
-                        </TableHead>
-                    ))}
+                    <TableHead className="w-[21%] text-nowrap text-[15px]">
+                        Amount <SortIcon columnName="amount" sortConfig={sortConfig} requestSort={requestSort} />
+                    </TableHead>
+                    <TableHead className="w-[20%] text-nowrap text-[15px]">
+                        Currency <SortIcon columnName="currency" sortConfig={sortConfig} requestSort={requestSort} />
+                    </TableHead>
+                    <TableHead className="w-[25%] text-nowrap text-[15px]">
+                        Cardholder <SortIcon columnName="cardholder" sortConfig={sortConfig} requestSort={requestSort} />
+                    </TableHead>
+                    <TableHead className="w-[24%] text-nowrap text-[15px]">
+                        Status <SortIcon columnName="status" sortConfig={sortConfig} requestSort={requestSort} />
+                    </TableHead>
+                    <TableHead className="w-[9%] text-nowrap text-[15px] pr-0">
+                        Created <SortIcon columnName="created_at" sortConfig={sortConfig} requestSort={requestSort} />
+                    </TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
